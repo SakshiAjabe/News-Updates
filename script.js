70428886ab4e76a75c9f14966343d4c103750b94
Sakshi Ajabe <sakshiajabe4@gmail.com>
2024-12-24 12:23:50 +0530

    news app

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "abae3ac4197c47b9bf5dab489bdb9435";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
